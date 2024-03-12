c566247ebae03cdcd1fc652559274ef1b4965547
leonardooliveirati <78557233+leonardooliveirati@users.noreply.github.com>
2024-03-12 15:15:41 -0300

    Ajuste calculo melhor rota

--- src/app/melhor-rota/melhor-rota.component.ts
+++ src/app/melhor-rota/melhor-rota.component.ts
@@ -1,12 +1,31 @@
+// rota-form.component.ts
+
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-melhor-rota',
-  standalone: true,
-  imports: [],
+  selector: 'app-rota-form',
   templateUrl: './melhor-rota.component.html',
-  styleUrl: './melhor-rota.component.css'
+  styleUrls: ['./melhor-rota.component.css']
 })
-export class MelhorRotaComponent {
+export class RotaFormComponent {
 
+  origem: string = "";
+  destino: string = "";
+  melhorRota: string = "";
+
+  constructor(private http: HttpClient) { }
+
+  buscarMelhorRota(): void {
+    const url = `http://localhost:porta/api/rotas/melhor-rota?origem=${this.origem}&destino=${this.destino}`;
+
+    this.http.get<string>(url).subscribe(
+      (rota) => {
+        this.melhorRota = rota;
+      },
+      (error) => {
+        console.error('Erro ao buscar a melhor rota:', error);
+      }
+    );
+  }
 }
